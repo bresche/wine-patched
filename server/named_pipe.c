39c7121614943516e2a38342538bac12c5f3d2af
Sebastian Lackner <sebastian@fds-team.de>
2015-12-04 10:36:47 +0100

    server: Introduce a new alloc_handle object callback. (v2)
    Signed-off-by: Sebastian Lackner <sebastian@fds-team.de>

--- server/named_pipe.c
+++ server/named_pipe.c
@@ -138,6 +138,7 @@ static const struct object_ops named_pipe_ops =
     named_pipe_link_name,         /* link_name */
     default_unlink_name,          /* unlink_name */
     named_pipe_open_file,         /* open_file */
+    no_alloc_handle,              /* alloc_handle */
     no_close_handle,              /* close_handle */
     named_pipe_destroy            /* destroy */
 };
@@ -178,6 +179,7 @@ static const struct object_ops pipe_server_ops =
     no_link_name,                 /* link_name */
     NULL,                         /* unlink_name */
     no_open_file,                 /* open_file */
+    no_alloc_handle,              /* alloc_handle */
     fd_close_handle,              /* close_handle */
     pipe_server_destroy           /* destroy */
 };
@@ -224,6 +226,7 @@ static const struct object_ops pipe_client_ops =
     no_link_name,                 /* link_name */
     NULL,                         /* unlink_name */
     no_open_file,                 /* open_file */
+    no_alloc_handle,              /* alloc_handle */
     fd_close_handle,              /* close_handle */
     pipe_client_destroy           /* destroy */
 };
@@ -272,6 +275,7 @@ static const struct object_ops named_pipe_device_ops =
     directory_link_name,              /* link_name */
     default_unlink_name,              /* unlink_name */
     named_pipe_device_open_file,      /* open_file */
+    no_alloc_handle,                  /* alloc_handle */
     fd_close_handle,                  /* close_handle */
     named_pipe_device_destroy         /* destroy */
 };
